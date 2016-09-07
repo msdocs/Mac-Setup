90a6a7f3ce6427481d1e1dfaef3217fb13038e88
Mahmoud Saleh <msdocs.ms@gmail.com>
2016-09-07 14:14:29 -0400

    hello

---
+++ mac/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Check if Homebrew is Installed  & install it if not
+if ! which brew ;
+then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+# Install Brew Programs
+for i in $( cat list/brew ); do
+   brew install $i
+done
+# Install Caskroom & Configure versions of apps
+brew cask
+rm /usr/local/Library/Taps/caskroom/homebrew-cask/Casks/expandrive.rb
+cp expandrive.rb /usr/local/Library/Taps/caskroom/homebrew-cask/Casks/expandrive.rb
+# Installs Brew Cask Apps
+brew tap caskroom/versions
+for i in $( cat list/cask ); do
+   brew cask install $i
+done
+for i in $( cat list/atom ); do
+   brew install $i
+done
+# Cleanup
+brew cleanup
+brew cask cleanup
